6d538e87ea90db3958b50f578761c23f72d12d01
maurelio-web <m.aurelio@escola.pr.gov.br>
2026-08-27 17:58:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// O efeito de virar o card é feito pelo CSS.
+// Este arquivo pode ser usado para adicionar
+// outras funcionalidades ao projeto futuramente.
+
+console.log("Confere Aí carregado com sucesso!");
